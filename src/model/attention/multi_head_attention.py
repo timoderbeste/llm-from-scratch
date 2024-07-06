Fixing the GPT2 implementation by fixing the boolean mask for causal attention.

src/model/attention/multi_head_attention.py
@@ -31,7 +31,7 @@ class MultiHeadAttention(nn.Module):
 
         self.register_buffer(
             'mask',
-            torch.tril(torch.ones(context_length, context_length), diagonal=1)
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
         )
 
     def forward(self, x):
@@ -98,7 +98,7 @@ class MultiHeadAttentionWithCombinedQKV(nn.Module):
 
         self.register_buffer(
             'mask',
-            torch.tril(torch.ones(context_length, context_length), diagonal=1)
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
         )
 
     def forward(self, x):

src/model/block/transformer_block.py
@@ -2,6 +2,7 @@ from torch import nn
 
 from src.model.attention.multi_head_attention import MultiHeadAttention
 from src.model.layer.feed_forward_equal import FeedForwardEqual
+from src.model.layer.layer_normalization import LayerNormalization
 
 
 class TransformerBlock(nn.Module):
@@ -27,21 +28,21 @@ class TransformerBlock(nn.Module):
             qkv_bias=qkv_bias,
         )
         self.ff = FeedForwardEqual(emb_dim, hidden_size)
-        self.norm1 = nn.LayerNorm(emb_dim)
-        self.norm2 = nn.LayerNorm(emb_dim)
-        self.drop_shortcut = nn.Dropout(dropout)
+        self.norm1 = LayerNormalization(emb_dim)
+        self.norm2 = LayerNormalization(emb_dim)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         shortcut = x
         x = self.norm1(x)
         x = self.attn(x)
-        x = self.drop_shortcut(x)
+        x = self.dropout(x)
         x = x + shortcut
 
         shortcut = x
         x = self.norm2(x)
         x = self.ff(x)
-        x = self.drop_shortcut(x)
+        x = self.dropout(x)
         x = x + shortcut
 
         return x

src/model/layer/gelu.py
@@ -9,5 +9,5 @@ class GELU(nn.Module):
 
     def forward(self, x):
         return 0.5 * x * (1 + torch.tanh(
-            torch.sqrt(torch.tensor(2.0 / torch.pi) * (x + 0.044715 * torch.pow(x, 3)))
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))
         ))

src/model/llm/gpt2.py
@@ -14,7 +14,12 @@ class GPT2Model(nn.Module):
         self.drop_emb = nn.Dropout(config['dropout'])
 
         self.transformer_blocks = nn.Sequential(
-            *[TransformerBlock(config['emb_dim'], config['emb_dim'], config['num_heads']) for _ in
+            *[TransformerBlock(
+                config['emb_dim'],
+                config['emb_dim'],
+                config['num_heads'],
+                config['dropout']
+            ) for _ in
               range(config['num_layers'])],
         )
         self.final_norm = LayerNormalization(config['emb_dim'])